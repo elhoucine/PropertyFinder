e67d57a0f5291591776b152defeef2d8139b5ffe
Elhoucine <houcineparade@gmail.com>
2016-03-26 10:36:58 +0000

    add styles

--- searchResults.js
+++ searchResults.js
@@ -45,4 +45,32 @@ class SearchResults extends Component {
   }
 }
 
+var styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+});
+
 module.exports = SearchResults;
